5c53dca9edbf508ed50fbb3155155f62b8225f28
Rahman Yasir Ates <rahmanyasir.ates@nttdata.com>
2024-12-17 12:13:44 +0000

    yedek

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zrecolang.zrecolang.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
